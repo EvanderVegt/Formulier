d5370ce3ef5532dc7e306a00f4c239b4c9cc889e
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-10 15:34:36 +0100

    stap1

---
+++ Welkom_get.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>welkom</title>
+    </head>
+    <body>
+        Welkom <?php echo $_POST["naam"]; ?><br>
+        Je woonadres is: <?php echo $_POST["adres"]; ?>
+
+    </body>
+</html>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Invoer</title>
+    </head>
+    <body>
+        <form action="Welkom_get.php" method="post">
+            Naam: <input type="text" name="naam"><br>
+            Adres: <input type="text" name="adres"><br>
+            <input type="submit">
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
