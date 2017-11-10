26f71aee682c383bb300ef1a5109b8cc1e202d15
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-10 16:09:41 +0100

    stap2

--- index.php
+++ index.php
@@ -7,12 +7,21 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Invoer</title>
+        <title>Contactformulier</title>
     </head>
     <body>
         <form action="Welkom_get.php" method="post">
-            Naam: <input type="text" name="naam"><br>
-            Adres: <input type="text" name="adres"><br>
+            <br><br>
+            Naam    : <input type="text" name="naam"><br><br>
+            E-mail  : <input type="text" name="adres"><br><br>
+            Geslacht: <select>
+                <option value="Man">Man</option>
+                <option value="Vrouw">Vrouw</option>
+            </select><br><br><br><br>
+            Telefoon  : <input type="text" name="telefoon" maxlength="10" size="7"><br><br>
+            Website   : <input type="text" name="website"><br><br>
+            Bericht   : <textarea name"textfield" cols="50" rows="3" style=width: 300px; height: 200px;"></textarea><br><br>
+            
             <input type="submit">
         </form>
         <?php
