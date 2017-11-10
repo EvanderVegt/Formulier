393f6ad418fdc316820685db39e945e3871f5a99
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-10 17:16:31 +0100

    stap3

--- Welkom_get.php
+++ Welkom_get.php
@@ -1,11 +1,25 @@
 <html>
-    <head>
-        <title>welkom</title>
-    </head>
-    <body>
-        Welkom <?php echo $_POST["naam"]; ?><br>
-        Je woonadres is: <?php echo $_POST["adres"]; ?>
+        <head>  
+            <style>
 
+                       body{
+
+                background-color: lightpink
+                
+            }
+            </style>
+            <title>Verzonden</title>
+            <h1>Ontvangen contactformuliergegevens</h1>
+        </head>
+    <body>
+
+    Naam:  <?php echo$_POST["naam"]; ?><br>
+    Woonadres: <?php echo $_POST["adres"]; ?><br>
+    Geslacht: <?php echo $_POST["geslacht"]; ?><br>
+    Telefoonnummer: <?php echo $_POST["telefoon"]; ?><br>
+    Website: <?php echo $_POST["website"]; ?><br>
+    Bericht: <?php echo $_POST["textfield"]; ?><br>
+    
     </body>
 </html>
 
--- index.php
+++ index.php
@@ -6,23 +6,36 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <style>
+            body{
+            background-color:powderblue;
+            }
+
+            
+            input,textarea,select{
+            background-color:lightblue;
+            }
+           
+            </style>
+    
         <title>Contactformulier</title>
+        <h1>Contactformulier</h1>
+        
     </head>
-    <body>
+   <body>
         <form action="Welkom_get.php" method="post">
             <br><br>
             Naam    : <input type="text" name="naam"><br><br>
             E-mail  : <input type="text" name="adres"><br><br>
-            Geslacht: <select>
+            Geslacht: <select name="geslacht">
                 <option value="Man">Man</option>
                 <option value="Vrouw">Vrouw</option>
             </select><br><br><br><br>
             Telefoon  : <input type="text" name="telefoon" maxlength="10" size="7"><br><br>
             Website   : <input type="text" name="website"><br><br>
-            Bericht   : <textarea name"textfield" cols="50" rows="3" style=width: 300px; height: 200px;"></textarea><br><br>
-            
-            <input type="submit">
+            Bericht   : <textarea name="textfield" cols="50" rows="3" style="width: 300px; height: 50px;"></textarea><br><br>
+                        <input type="submit" value="Verzenden" id="button">
+      
         </form>
         <?php
         // put your code here
